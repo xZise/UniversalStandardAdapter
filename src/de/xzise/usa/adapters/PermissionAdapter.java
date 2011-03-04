6400cca2c2d1fa5769ae141b52d68ae992b3e7d4
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-03-04 10:59:01 +0100

    +new permission adapter (which support data for a permission); !hasPermission added the permission name;

--- src/de/xzise/usa/adapters/PermissionAdapter.java
+++ src/de/xzise/usa/adapters/PermissionAdapter.java
@@ -4,7 +4,7 @@ import org.bukkit.command.CommandSender;
 
 public interface PermissionAdapter extends Adapter {
 
-	boolean hasPermission(CommandSender sender);
+	boolean hasPermission(CommandSender sender, String name);
 	
 	String getGroup(String name);
 	
---
+++ src/de/xzise/usa/adapters/PermissionDataAdapter.java
@@ -0,0 +1,10 @@
+package de.xzise.usa.adapters;
+
+public interface PermissionDataAdapter extends PermissionAdapter {
+
+	int getPermissionInteger(String user, String name);
+	double getPermissionDouble(String user, String name);
+	String getPermissionString(String user, String name);
+	boolean getPermissionBoolean(String user, String name);
+	
+}
