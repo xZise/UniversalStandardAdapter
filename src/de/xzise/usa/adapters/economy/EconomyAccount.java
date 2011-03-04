f5868e747b7fa2d5e728abec6a8afcd691615eab
Zren <zrenfire@gmail.com>
2011-03-04 07:10:04 -0500

    Few economy things.

--- src/de/xzise/usa/adapters/economy/EconomyAccount.java
+++ src/de/xzise/usa/adapters/economy/EconomyAccount.java
@@ -3,6 +3,7 @@ package de.xzise.usa.adapters.economy;
 public interface EconomyAccount {
 	boolean canPay(int amount);
 	
-	void transferMoney(int amount);
-	void transferMoney(int amount, EconomyAccount target);
+	void transferMoney(int amount) throws EconomyException;
+	void transferMoney(int amount, EconomyAccount target) throws EconomyException;
+	String formatMoney(int amount);
 }
---
+++ src/de/xzise/usa/adapters/economy/EconomyException.java
@@ -0,0 +1,5 @@
+package de.xzise.usa.adapters.economy;
+
+public class EconomyException extends Exception {
+	private static final long serialVersionUID = -1561369505330521552L;
+}
